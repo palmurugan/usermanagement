c622ee82a4be60922259087f6762c2fb19c665d7
Pal Murugan C <palmuruganc@suntecgroup.com>
2019-02-28 17:49:07 +0530

    Initial Commit

---
+++ validator/src/main/java/org/validator/App.java
@@ -0,0 +1,13 @@
+package org.validator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
